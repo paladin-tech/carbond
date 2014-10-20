2f2c62ab03fcd01446b0ad617ac1185c88674871
mixailo <mixailom@gmail.com>
2014-10-20 13:54:47 +0200

    db connection updated

--- protected/config/console.php
+++ protected/config/console.php
@@ -11,19 +11,20 @@ return array(
 
 	// application components
 	'components'=>array(
+	/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		),*/
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host= mysql507.loopia.se;dbname=beacon189_com_db_2',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'carbond@b21281',
+			'password' => 'C4B14BcN11',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(

9a601b20b5f776b7be2235720b0dab23453b3072
paladin-tech <johnsie@gmail.com>
2014-10-21 23:26:35 +0200

    main layout

--- themes/carBond/views/layouts/main.php
+++ themes/carBond/views/layouts/main.php
@@ -38,31 +38,32 @@ $cs
     <![endif]-->
 </head>
 
-<body>
+<body class="<?php echo Yii::app()->controller->getId() . ' ' . Yii::app()->controller->action->id; ?>">
 <header>
-	<div class="container">
-		<div class="row">
+	<div class="container-fluid">
+		<div class="row ">
             <div class="col-lg-2"><h1 id="logotipo"><a href="#"><?php echo CHtml::encode(Yii::app()->name); ?></a></h1></div>
-            <div class="col-lg-8 col-xs-12">
-                <div id="banner"><img src="<?php print $basePath.'/images/banner_top.png'; ?>" class="img-responsive" width="100%" height="100%"/></div>
+            <div class="col-lg-8 col-xs-12 ">
+                <div class="fixed-layout-header"><div id="banner" ><img src="<?php print $basePath.'/images/banner_top.png'; ?>" class="img-responsive" width="100%" height="100%"/></div>
                 <?php $this->widget('zii.widgets.CMenu', array(
 		            'id'    => 'menu',
 		            'items' => array(
 			            array('label' => 'Search by VIN', 'url' => array('/vehicle/searchByVin'), 'itemOptions' => array('class' => 'first'),),
 			            array('label' => 'Vehicle Sale', 'url' => array('/vehicleAdvertisment/create', 'vehicleTypeId' => 1)),
 			            array('label' => 'Search', 'url' => array('/vehicleAdvertisment/index', 'vehicleTypeId' => 1)),
-			            array('label' => 'Servicers', 'url' => array('/servicer/brands')),
 			            array('label' => 'Servicing Data', 'url' => array('/servicer/addServicingData', 'vehicleTypeId' => 1), 'visible' => (!Yii::app()->user->isGuest && Yii::app()->user->getState('userRoles')['isOfficialService'])),
-			            array('label' => 'Damage Data', 'url' => array('/insuranceHouse/addDamageData', 'vehicleTypeId' => 1), 'visible' => (!Yii::app()->user->isGuest && Yii::app()->user->getState('userRoles')['isInsuranceHouse'])),
 			            array('label' => 'Login', 'url' => array('/site/login'), 'itemOptions' => array('class' => 'last'), 'visible' => Yii::app()->user->isGuest),
 			            array('label' => 'Logout', 'itemOptions' => array('class' => 'last'), 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
 		            ),
-	            )); ?>
+	            )); ?></div>
             </div>
             <div class="col-lg-2 col-xs-12">
-            	<a href="#" class="tool1"><span><img src="<?php print $basePath.'/images/opsta-zupcanik.png'; ?>"/></span>Opsta pretraga</a>
-                <a href="#" class="tool2"><span><img src="<?php print $basePath.'/images/detaljna-zupcanik.png'; ?>"/></span>Detaljna pretraga</a>
-                <a href="#" class="tool3">Unesite podatke ...></a>
+            	<!--<a href="#" class="tool1"><span><img src="<?php //print $basePath.'/images/opsta-zupcanik.png'; ?>"/></span>Opsta pretraga</a>
+                <a href="#" class="tool2"><span><img src="<?php //print $basePath.'/images/detaljna-zupcanik.png'; ?>"/></span>Detaljna pretraga</a>
+                <a href="#" class="tool3">Unesite podatke ...></a>-->
+                <a href="#" class="language-swap">Srp</a>
+                <a href="#">Log-in</a>
+                <a href="#">Registracija</a>
                 <form class="search">
                     <div class="input-group">
                     	<input type="text" autocomplete="off" placeholder="Trazite pobroju sasije..." class="form-control query" name="q" id="query">
@@ -77,19 +78,25 @@ $cs
     </div>
 
 </header>
-<div class="wrap" id="teste">
-	<div class="faixa-branca clearfix">
-	</div>
-	<div class="container principal">
+<div class="wrap" >
+	<!--<div class="faixa-branca clearfix">
+	</div>-->
+	<div class="container-fluid principal">
 		<div class="row">
 	        <section class="col-md-12">
-	            <?php echo $content; ?>
+            	<div class="row">
+                	<div class="col-md-2"></div>
+                    	<div class="col-md-8">
+	            		<div class="fixed-layout-content"><?php echo $content; ?></div>
+                        </div>
+                	<div class="col-md-2"></div>
+                </div>
 	        </section>
 	    </div>
 	</div>
 </div>
 <footer id="footer" class="clearfix">
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-4">
 				<ul class="redes-sociais">
